9c3a38048f2b208d60578c170bfd7268254d9734
gustavobgt <gustavo_bgt97@hotmail.com>
2023-07-16 16:57:54 -0300

    feat: finish all pages

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,11 +1,11 @@
-import React from "react";
 import { motion } from "framer-motion";
+import React from "react";
 
 const quote = {
-  initial: {
+  hidden: {
     opacity: 1,
   },
-  animate: {
+  visible: {
     opacity: 1,
     transition: {
       delay: 0.5,
@@ -15,11 +15,11 @@ const quote = {
 };
 
 const singleWord = {
-  initial: {
+  hidden: {
     opacity: 0,
     y: 50,
   },
-  animate: {
+  visible: {
     opacity: 1,
     y: 0,
     transition: {
@@ -30,22 +30,26 @@ const singleWord = {
 
 const AnimatedText = ({ text, className = "" }) => {
   return (
-    <div className="w-full mx-auto py-2 items-center justify-center text-center overflow-hiddenF">
+    <div className="py-2 w-full mx-auto flex flex-col items-center justify-center  text-center  
+    overflow-hidden sm:py-0">
       <motion.h1
-        className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className}`}
+        className={`inline-block text-dark dark:text-light
+      text-8xl font-bold w-full capitalize  ${className} xl:text-6xl`}
         variants={quote}
-        initial="initial"
-        animate="animate"
+        initial="hidden"
+        animate="visible"
       >
-        {text.split(" ").map((word, index) => (
-          <motion.span
-            variants={singleWord}
-            key={word + "-" + index}
-            className="inline-block"
-          >
-            {word}&nbsp;
-          </motion.span>
-        ))}
+        {text.split(" ").map((char, index) => {
+          return (
+            <motion.span
+              className="inline-block"
+              key={char + "-" + index}
+              variants={singleWord}
+            >
+              {char}&nbsp;
+            </motion.span>
+          );
+        })}
       </motion.h1>
     </div>
   );
--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,16 +1,21 @@
+import Link from "next/link";
 import React from "react";
 import { CircularText } from "./Icons";
-import Link from "next/link";
 
-const HireMe = () => {
+export const HireMe = () => {
   return (
-    <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden">
-      <div className="w-48 h-auto flex items-center justify-center relative">
-        <CircularText className={"fill-dark animate-spin-slow"} />
-
+    <div className="fixed left-4 bottom-4 flex flex-col items-center justify-center md:right-8 sm:right-0 
+    overflow-hidden md:bottom-auto md:left-auto md:top-0 md:absolute z-20">
+      <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
+        <CircularText
+          className={"fill-dark dark:fill-light animate-spin-slow duration-200"}
+        />
         <Link
-          href="mailto:contato@gustavotx.dev"
-          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark"
+          href="mailto:codebucks27@gmail.com"
+          className="flex items-center justify-center absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] shadow-md border border-solid
+bg-dark rounded-full text-white w-20 h-20 font-semibold hover:bg-light hover:border-dark hover:text-dark dark:text-dark dark:bg-light dark:hover:bg-dark
+dark:hover:text-light dark:hover:border-light dark:shadow-light/25 md:w-12 md:h-12 md:text-[10px]
+"
         >
           Hire Me
         </Link>
@@ -18,5 +23,3 @@ const HireMe = () => {
     </div>
   );
 };
-
-export default HireMe;
